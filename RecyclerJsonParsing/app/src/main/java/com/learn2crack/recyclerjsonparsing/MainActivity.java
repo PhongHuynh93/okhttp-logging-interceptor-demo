a909c187347689a67daa9794cd40f7eab604555c
Phong <p7157568@gmail.com>
2016-08-14 14:27:31 +0700

    feat - add steps for create the okhttp intercept

--- RecyclerJsonParsing/app/src/main/java/com/learn2crack/recyclerjsonparsing/MainActivity.java
+++ RecyclerJsonParsing/app/src/main/java/com/learn2crack/recyclerjsonparsing/MainActivity.java
@@ -17,6 +17,18 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+/**
+ *
+ * @see <a href="https://www.learn2crack.com/2016/06/retrofit-okhttp-logging-interceptor.html"></a>
+ * Sometimes while working with Retrofit and parsing JSON data you may get some weird errors similar to this,
+ * Error :JsonReader.setLenient(true)to accpect malformed JSON at line 1 Column 1 path $
+ * The problem is the JSON response is not valid so that the Gson converter cannot serialize it.
+ *
+ * -> ta phải có cách để xem JSON tra về, có 2 cách
+ * 1. One is to use Postman to make the request and check the response
+ * 2. the other is to attach Okhttp logging interceptor to Retrofit and check the logs.
+ *
+ */
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
@@ -27,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // TODO: 8/14/16 2 when oncreate, init view
         initViews();
     }
     private void initViews(){
@@ -34,17 +47,23 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setHasFixedSize(true);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
         recyclerView.setLayoutManager(layoutManager);
+        // TODO: 8/14/16 3 should be onstart to load the connection 
         loadJSON();
     }
 
     private void loadJSON(){
-
+        // TODO: 8/14/16 4 create client 
+        OkHttpClient.Builder client = new OkHttpClient.Builder();
+        
+        // TODO: 8/14/16 5 create a new HttpLoggingInterceptor and set logging level,
+        // BODY prints everything.
         HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
         loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
 
-        OkHttpClient.Builder client = new OkHttpClient.Builder();
-
+        // TODO: 8/14/16 6 Then add the interceptor to Okhttp client,
         client.addInterceptor(loggingInterceptor);
+
+        // TODO: 8/14/16 7 Finally add the Okhttp client to Retrofit using client() method.
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("http://api.learn2crack.com")
                 .client(client.build())
